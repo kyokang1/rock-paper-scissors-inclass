6866d4341a498c74da2dfded3ac4d500f6e14fca
kyokang1 <kyoungmokang@gmail.com>
2019-06-05 18:56:32 -0400

    Capture User Input

--- game.py
+++ game.py
@@ -1,6 +1,26 @@
 # game.py
 
-print("Rock, Paper, Scissors, SHOOT!")
+print("Rock, Paper, Scissors, SHOOT!")  # this is also a comment
+
+# Capture Inputs
+
+user_choice = input("Please choose one of the following options: 'rock', 'paper', or 'scissors' (without the quotes):")
+
+print("------------")
+print("You chose:", user_choice)
+print("------------")
+
+# VALIDATE INPUTS
+
+
+# Generate COmputer selection
+
+
+# Determine the winer
+
+
+
+# Display final outputs/outcomes
 
 
 
